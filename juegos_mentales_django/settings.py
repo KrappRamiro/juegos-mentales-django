2c7db6e3829480b2f2fa1e6a38bb6a185bf417e6
Krapp Ramiro <krappramiro@disroot.org>
2023-02-17 13:54:00 -0300

    add postgres

--- juegos_mentales_django/settings.py
+++ juegos_mentales_django/settings.py
@@ -78,10 +78,21 @@ WSGI_APPLICATION = 'juegos_mentales_django.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'jm_hannibal',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
